ac591095b6ef8dc8391dd5023b300a1f28f61167
Jacob Fratti <JacobRFratti@gmail.com>
2023-11-07 20:30:26 -0500

    Get api working. Thank cathulu

--- server.js
+++ server.js
@@ -17,27 +17,19 @@ app.get('/notes', (req, res) => {
 });
 
 // Gather notes data for get and post requests
-var notesData = getNotes();
-
 function getNotes() {
     let data = fs.readFileSync('./db/db.json', 'utf8');
-
     let notes = JSON.parse(data);
-
-    // Give each note an ID that matches its index (this gets run for every time the page is refreshed)
     for (let i = 0; i < notes.length; i++) {
         notes[i].id = '' + i;
     }
-
     return notes;
-}
+};
 
 // Read the db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
-    res.json(fs.readFile('db/db.json', (err, data) => { //none of this works, find a better way
-        if (err) throw err;
-        const db = JSON.parse(data);
-    }))
+    console.log(getNotes());
+    res.json(getNotes());
 });
 
 //Receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved
